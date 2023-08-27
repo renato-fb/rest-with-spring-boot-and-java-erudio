9f2100d51d83d238a99d3c0b4a95239274f9e8a3
Renato <renatofb64@gmail.com>
2023-08-27 20:48:10 -0300

    Update

---
+++ 07_APIVerbsWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/renato/services/PersonServices.java
@@ -0,0 +1,47 @@
+package br.com.renato.services;
+
+import br.com.renato.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("Costa");
+        person.setAddress("Uberlândia - Minas Gerais - Brasil");
+        person.setGender("Male");
+        logger.info("Finding one person!");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<>();
+        for(int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brasil " + i);
+        person.setGender("");
+        return person;
+    }
+}
