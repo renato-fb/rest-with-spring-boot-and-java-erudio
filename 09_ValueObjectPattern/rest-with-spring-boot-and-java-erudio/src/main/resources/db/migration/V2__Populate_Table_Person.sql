c1a6ab5c5b951b6df416024f6227b6353047343b
Renato <renatofb64@gmail.com>
2023-09-25 17:52:04 -0300

    Code

---
+++ 09_ValueObjectPattern/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` VALUES (1,'Some address in Brasil 0','Person name 0','Male','Last name 0'),
+(2,'Some address in Brasil 0','Person name 0','Male','Last name 0'),
+(3,'Some address in Brasil 0','Person name 0','Male','Last name 0'),
+(4,'Some address in Brasil 0','Person name 0','Male','Last name 0'),
+(5,'Mvezo - South Africa','André','Male','Moreira'),
+(6,'Mvezo - South Africa','Nicola','Male','Tesla'),
+(7,'Mvezo - South Africa','Nicola','Male','Tesla'),
+(8,'Mvezo - South Africa','Nicola','Male','Tesla');
+
