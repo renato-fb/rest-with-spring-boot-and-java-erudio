cede4fb0f13a52363575c47cfe6b87481cd3ce41
Renato <renatodefreitas_@hotmail.com>
2023-10-04 13:37:40 -0300

    HATEOAS implementation

--- 12_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/renato/util/MediaType.java
+++
@@ -1,7 +0,0 @@
-package br.com.renato.util;
-
-public class MediaType {
-    public static final String APPLICATION_JSON = "application/x-json";
-    public static final String APPLICATION_XML = "application/x-xml";
-    public static final String APPLICATION_YML = "application/x-yaml";
-}
